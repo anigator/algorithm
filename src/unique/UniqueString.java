46ea5139aa1f3714ee08e5171681a6e182718c6b
Gold(나정우) <jwna@coupang.com>
2017-05-31 19:00:20 +0900

    uniqueString commit

---
+++ src/unique/UniqueString.java
@@ -0,0 +1,24 @@
+package unique;
+
+import java.util.HashMap;
+
+public class UniqueString {
+
+	public static boolean isUniqueChar(String str) {
+		HashMap<String, Boolean> map = new HashMap<>();
+		int strLength = str.length();
+		for (int i=0; i < strLength; i++) {
+			String charString = String.valueOf(str.charAt(i));
+			if (map.containsKey(charString)) {
+				return false;
+			}
+			map.put(String.valueOf(str.charAt(i)), true);
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String str ="abcdefghijkla";
+		System.out.println(isUniqueChar(str));
+	}
+}
